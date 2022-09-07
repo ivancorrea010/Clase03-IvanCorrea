b843f08ee419524a2dd7a50ed8910c582503df34
ivancorrea010 <103391608+ivancorrea010@users.noreply.github.com>
2022-09-07 16:00:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let numero = Number(prompt('Ingrese el numero de saludos que quiere visualizar (Podras tener un máximo de 6 saludos'))
+
+for (let i = 1; i <= numero; i++) {
+    alert('Hola (nro:'+i+')')
+    if(i===6){
+        break;
+    }
+ 
+    
+
+}
